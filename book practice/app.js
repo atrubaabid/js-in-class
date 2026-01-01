11b4c79ea0bdda61fcdef4d2578a06d88f7139c6
atrubaabid <atrubaabid7@gmail.com>
2026-01-01 16:34:21 +0500

    work

---
+++ book practice/app.js
@@ -0,0 +1,93 @@
+// CHAP 49 READING FIELD VALUES
+
+
+// function checkAddress(fieldId) {
+//     var val = document.getElementById(fieldId).value;
+//     if (val === "") {
+
+//         alert("Email address required.");
+//     }
+// }
+
+
+
+// CHAP 50 Setting FIELD VALUES
+
+// function fillCity() {
+
+//     var cityName;
+
+//     var zipEntered = document.getElementById("zip").value;
+//     switch (zipEntered) {
+//         case "60608":
+//             cityName = "Chicago";
+//             break;
+//         case "68114":
+//             cityName = "Omaha";
+//             break;
+//         case "53212":
+
+//             cityName = "Milwaukee";
+//     }
+//     document.getElementById("city").value = cityName;
+// }
+
+// function Auto() {
+//     let cityname;
+//     let zip = document.getElementById("zip").value;
+//     let city = document.getElementById("city");
+//     switch (zip) {
+//         case "10":
+//             cityname = "Karachi"
+//             break;
+//         case "20":
+//             cityname = "Islamabad"
+//             break;
+//         case "30":
+//             cityname = "lahore"
+//             break;
+//         case "40":
+//             cityname = "punjab"
+//             break;
+
+//     }
+
+//     city.value = cityname;
+
+// }
+
+
+//  CHAP 51 Reading and setting paragraph text
+
+
+
+
+
+
+
+// function more(){
+//     let para = document.getElementById("para");
+//     let text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. A et illo tempore voluptates, eum vero architecto sapiente commodi. Quod, aliquam!"
+//     para.innerHTML= text;
+// }
+
+
+
+// function more(){
+//     let para = document.getElementById("para");
+//     let text = "<ol><li>Slow loris</li><li>Fast loris</li><li>Just-right loris</li></ol>"
+//     para.innerHTML= text;
+// }
+
+
+
+// function more(){
+//     let para = document.getElementById("par").innerHTML;
+//     document.getElementById('para').innerHTML = para
+    
+    
+  
+// }
+
+
+
